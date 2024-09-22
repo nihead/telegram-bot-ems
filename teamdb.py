b8ade27936af98adc5b7e61109c7999c7feeb8b7
nohead <nih4ad@gmail.com>
2024-09-23 00:04:12 +0500

    is_db

--- teamdb.py
+++ teamdb.py
@@ -25,7 +25,7 @@ def create_table(conn, create_table_sql):
         print(e)
 
 def is_db():
-    return os.path.isfile("./team_ems.db")
+    return os.path.isfile("team_ems.db")
 
 
 def table_exists(table_name):
@@ -127,6 +127,4 @@ def main():
 
 
 if __name__ == '__main__':
-    if not is_db():
-        db_init()
-    print(read_all())
+    print(is_db())
