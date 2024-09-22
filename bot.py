181a32beb4cdea7a421e97dea9410cecc7b3ad70
nohead <nih4ad@gmail.com>
2024-09-22 23:59:02 +0500

    cid added

--- bot.py
+++ bot.py
@@ -2,25 +2,14 @@
 # pylint: disable=unused-argument
 # This program is dedicated to the public domain under the CC0 license.
 
-"""
-Simple Bot to reply to Telegram messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Application and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
 import logging
 import os
 
 from dotenv import load_dotenv
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+
+from teamdb import db_init
 
 # Enable logging
 logging.basicConfig(
@@ -58,6 +47,8 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 def main() -> None:
+    # creating database
+    db_init()
     """Start the bot."""
     load_dotenv()
 
--- teamdb.py
+++ teamdb.py
@@ -3,16 +3,17 @@ from sqlite3 import Error
 import os
 
 
-def create_connection(db_file):
+def create_connection():
     """ Create a database connection to a SQLite database """
     conn = None
+    c = None
     try:
-        conn = sqlite3.connect(db_file)
-        print(f"SQLite version: {sqlite3.version}")
-        return conn
+        conn = sqlite3.connect("team_ems.db")
+        c = conn.cursor()
+        return conn, c
     except Error as e:
         print(e)
-    return conn
+        return conn, c
 
 
 def create_table(conn, create_table_sql):
@@ -23,31 +24,109 @@ def create_table(conn, create_table_sql):
     except Error as e:
         print(e)
 
+def is_db():
+    return os.path.isfile("./team_ems.db")
+
+
+def table_exists(table_name):
+    # Connect to the SQLite database
+    conn = sqlite3.connect("team_ems.db")
+    cursor = conn.cursor()
+
+    # Query to check if the table exists in the database
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name=?", (table_name,))
+
+    # Fetch one result, if exists
+    table = cursor.fetchone()
+
+    # Close the connection
+    conn.close()
+
+    # Return True if table exists, else False
+    return table is not None
+
+
+def insert_new_user(user, conn = None) -> bool:
+    """ Insert a new user into the users table """
+    sql = ''' INSERT INTO users(uid, name, role)
+              VALUES(?,?,?) '''
+    try:
+        if not conn:
+            print(f"Connection makes {conn}")
+            conn = sqlite3.connect("team_ems.db")
+        cur = conn.cursor()
+        cur.execute(sql, user)
+        conn.commit()
+        return True
+    except Error as e:
+        print(f"Error occurred {e}")
+        return False
+    finally:
+        conn.close()
+
+def read_all():
+    conn, c = create_connection()
+    ###
+    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = c.fetchall()
+    print(tables)
+    ###
+
+    # curser = conn.cursor()
+    c.execute("SELECT * FROM users")
+    return c.fetchall()
+
+
+def db_init():
+    if not is_db():
+        db_init()
+    else:
+        print("Db found")
+
 
 def main():
-    database = "team_ems.db"
-
-    sql_create_users_table = """ CREATE TABLE IF NOT EXISTS users (
-                                    uid INTEGER PRIMARY KEY,
-                                    name TEXT NOT NULL,
-                                    role TEXT,
-                                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                    enrolled INTEGER DEFAULT 0,
-                                    attended INTEGER DEFAULT 0,
-                                    last_attended_at TIMESTAMP
-                                ); """
-
-    # Create a database connection
-    conn = create_connection(database)
-
-    # Create table
-    if conn is not None:
-        create_table(conn, sql_create_users_table)
-        print("Table 'users' created successfully.")
-        conn.close()
-    else:
+    # Tables
+    tables = [""" CREATE TABLE IF NOT EXISTS users (
+                                                uid INTEGER PRIMARY KEY,
+                                                name TEXT NOT NULL,
+                                                role TEXT,
+                                                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                                enrolled INTEGER DEFAULT 0,
+                                                attended INTEGER DEFAULT 0,
+                                                last_attended_at TIMESTAMP
+                                            );""",
+              """ CREATE TABLE IF NOT EXISTS schedules (
+                                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        place TEXT,
+                                        from_time TIME,
+                                        to_time TIME,
+                                        team_qty INTEGER,
+                                        reserve_qty INTEGER,
+                                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                                    ); """,
+              """ CREATE TABLE IF NOT EXISTS team_list (
+                                                      id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                                      uid INTEGER,
+                                                      sid INTEGER,
+                                                      on_team BOOLEAN,
+                                                      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                                      FOREIGN KEY (uid) REFERENCES users(uid),
+                                                      FOREIGN KEY (sid) REFERENCES schedules(id)
+                                                  ); """,
+              ]
+    try:
+        conn, c = create_connection()
+        for table in tables:
+            c.execute(table)
+        admin = insert_new_user((498123938, 'noHead', 'admin'))
+    except Error as e:
         print("Error! Cannot create the database connection.")
+        print(e)
+    finally:
+        conn.close()
 
 
 if __name__ == '__main__':
-    main()
+    if not is_db():
+        db_init()
+    print(read_all())
