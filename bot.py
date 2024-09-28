10242a178d0d530de7bc80a9199ac0a975ca3e14
nohead <nih4ad@gmail.com>
2024-09-28 14:41:21 +0500

    ems-v1

--- bot.py
+++ bot.py
@@ -7,13 +7,13 @@ import os
 
 from dotenv import load_dotenv
 from telegram import ForceReply, Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, CallbackQueryHandler, CallbackContext
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, CallbackQueryHandler, \
+    CallbackContext, ChatMemberHandler
 from pocketbase import PocketBase
 from services import Db
 import asyncio
 from threading import Thread
 import time
-
 
 # Enable logging
 logging.basicConfig(
@@ -24,18 +24,18 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
-#Keyboards
+# Keyboards
 kb_i_o = [
-        [
-            InlineKeyboardButton("IN", callback_data='IN'),
-            InlineKeyboardButton("OUT", callback_data='OUT')
-        ]
+    [
+        InlineKeyboardButton("IN", callback_data='IN'),
+        InlineKeyboardButton("OUT", callback_data='OUT')
     ]
+]
 kb_o = [
-        [
-            InlineKeyboardButton("OUT", callback_data='OUT')
-        ]
+    [
+        InlineKeyboardButton("OUT", callback_data='OUT')
     ]
+]
 
 
 # Define a few command handlers. These usually take the two arguments update and
@@ -47,18 +47,14 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     cid = update.message.chat.id
     welcome = f"""
     <b>WELCOME TO EMS FOOTBALL TEAM MAKER BOT!</b>
-    
+
 Salaam! {username},
-Please answer all question and wait for Approvals From Our Teams!!
+This bot is ....!!
 THANK YOU!
     """
     await update.message.reply_text(
         text=welcome,
         parse_mode='HTML'
-    )
-    await update.message.reply_html(
-        rf"What is your name?",
-        reply_markup=ForceReply(selective=True),
     )
 
 
@@ -70,71 +66,303 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 async def team_maker(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
     m = update.message.text.split('\n')
+    try:
+        mp = int(m[2])
+        mr = int(m[3])
+    except Exception as e:
+        print(e)
+        mp = 14
+        mr = 4
 
-    f_msg= f"""
+    try:
+        desc = m[1]
+    except Exception as e:
+        print(e)
+        desc = "Kulhun @ Male' city Council\nFrom <b>19:00</b> to <b>20:00</b>"
+
+    f_msg = f"""
     <u>Team List</u>
-<b>{m[1]}</b>
-    
-<i>Interested Press IN</i>
+<b>{desc}</b>
+---------------------
+<u><b>ONTEAM</b></u>
+---------------------
     """
 
-    await update.message.reply_text(
-        f_msg,
-        parse_mode='HTML',
-        reply_markup=InlineKeyboardMarkup(kb_i_o)
-    )
+    pb_db = Db()
+    await update.message.delete()
+    if pb_db.active_seesion():
+        f_msg = """<b>An active team listing session is currently in progress.</b>
+Please try again later.
+
+Thank you for your patience."""
+
+        await context.bot.send_message(chat_id=update.message.chat_id, text=f_msg, parse_mode="HTML")
+
+    else:
+        # create kulhun
+        await context.bot.send_message(
+            chat_id=update.message.chat_id,
+            text=f"Team listing session started with\nMax players = {mp}\nMax reserved = {mr}",
+            parse_mode="HTML",
+        )
+
+        await asyncio.sleep(2)
+
+        kulhun = await context.bot.send_message(
+            chat_id=update.message.chat_id,
+            text=f_msg,
+            parse_mode="HTML",
+            reply_markup=InlineKeyboardMarkup(kb_i_o)
+        )
+        pb_db.create_kulhun(update.message.from_user.id, desc, kulhun.message_id, mp, mr)
+
 
 # Define a function to handle button press events
 async def inline_button(update: Update, context) -> None:
     query = update.callback_query
     user = query.from_user  # Get the user who pressed the button
-    username = user.username or user.first_name
+    username = f"{user.first_name} {user.last_name if user.last_name else ''}"
     current_message = query.message.text
-    chat_id = query.message.chat.id
-
+    chat_id = query.from_user.id
 
     # Determine which button was pressed
-    if query.data == 'IN':
-        new_message = f"{current_message}\n{username} is: {query.data}"
-        suc = await add_to_db(chat_id)
-        if suc:
-            await query.edit_message_text(text=new_message, reply_markup=InlineKeyboardMarkup(kb_i_o))
-        else:
-            alert = await query.message.reply_text("May be you are on list")
-            print(alert.chat_id)
-            await asyncio.sleep(1)
-            await alert.delete()
+    try:
+        if query.data == 'IN':
+            # saving
+            suc, on_team, no_players, total_players = await add_to_db(chat_id, query, context)
 
-        # await query.edit_message_text(text="You pressed: IN")
-    elif query.data == 'OUT':
-        await query.edit_message_text(text="You pressed: OUT")
+            if suc:
+                pb_db = Db()
+                print("Making team list")
+                team_list = pb_db.team_list()
+                # get descrption from kulhun
+                desc = pb_db.pb.collection('kulhun').get_list(1, 20, {"filter": 'completed = false'}).items[
+                    0].description
+                team_msg = f"""
+<u>Team List</u>
+<i>{desc}</i>
+---------------------
+<u><b>ONTEAM</b></u>
+---------------------
+{team_list}
+"""
 
-    await query.answer()
+                await query.edit_message_text(text=team_msg, reply_markup=InlineKeyboardMarkup(kb_i_o),
+                                              parse_mode="HTML")
+                if no_players + 1 == total_players:
+                    await query.edit_message_reply_markup(reply_markup=InlineKeyboardMarkup(kb_o))
+            else:
+                alert = await query.message.reply_text("May be you are on list")
+                await asyncio.sleep(1)
+                await alert.delete()
+
+            # await query.edit_message_text(text="You pressed: IN")
+        elif query.data == 'OUT':
+
+            pb_db = Db()
+            if pb_db.on_list(chat_id) > 0:
+                await context.bot.send_message(
+                    chat_id=query.message.chat_id,
+                    text=f"{username}  takes a name off the list.",
+                    parse_mode="HTML"
+                )
+                # await query.edit_message_reply_markup(reply_markup=None)
+                await query.delete_message()
+
+                # mark user out from list & updating from reserved
+                suc = pb_db.off_list(chat_id)
+
+                # recreate team list
+                if suc:
+                    print("Making team list")
+                    team_list = pb_db.team_list()
+                    # get descrption from kulhun
+                    desc = pb_db.pb.collection('kulhun').get_list(1, 20, {"filter": 'completed = false'}).items[
+                        0].description
+                    team_msg = f"""
+<u>Team List</u>
+<i>{desc}</i>
+---------------------
+<u><b>ONTEAM</b></u>
+---------------------
+{team_list}
+"""
+                    new_list = await context.bot.send_message(
+                        chat_id=query.message.chat_id,
+                        text=team_msg,
+                        parse_mode="HTML",
+                        reply_markup=InlineKeyboardMarkup(kb_i_o)
+                    )
+
+                    # update new message id
+                    pb_db.pb.collection('kulhun').update(
+                        pb_db.pb.collection('kulhun').get_list(1, 20, {"filter": 'completed = false'}).items[0].id,
+                        {"message_id": new_list.message_id})
+
+
+            else:
+                warning = await context.bot.send_message(
+                    chat_id=query.message.chat_id,
+                    text=f"{username},\nyou are not on the list.\n{chat_id}",
+                    parse_mode="HTML"
+                )
+                await asyncio.sleep(2)
+                await warning.delete()
+
+    except Exception as e:
+        print(f"Call back error\n {e}")
+    finally:
+        await query.answer()
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
-    await update.message.reply_text(update.message.text)
+    if Db().active_seesion():
+        await update.message.delete()
+        alert = await update.message.reply_text("Ongoing Team polling\nNot allowed send messages")
+        await asyncio.sleep(2)
+        await alert.delete()
+    else:
+        await update.message.reply_text(update.message.text)
 
 
-def delete_alert(alert):
-    time.sleep(3)
-    alert.delete()
-
-async def add_to_db(tid):
+async def add_to_db(tid, query, context):
     pb = Db()
-    if pb.on_list(tid) == 0:
-        suc = pb.add_to_team(tid)
-        print("New player saved")
-        return True if suc else False
+    no_players = pb.no_players()
+    max_players = pb.max_players()
+    max_reserves = pb.max_reserved()
+    total_players = max_players + max_reserves
+    print(no_players, max_players, max_reserves, total_players)
 
-async def on_list(tid=498123938) -> int:
+    # players limit check
+    if pb.on_list(tid) == 0:
+        if no_players < total_players:
+            if no_players < max_players:
+                if pb.add_to_team(tid, True):
+                    print("New player saved")
+                    # return succeeded , on_team, no_players, t_player
+                    return True, True, no_players, total_players
+            else:
+                if pb.add_to_team(tid, False):
+                    print("New player saved")
+                    # return succeeded , on_team, no_players
+                    return True, False, no_players, total_players
+        else:
+            print("Max players reached")
+            await context.bot.send_message(
+                chat_id=query.message.chat_id,
+                text="Max players reached",
+                parse_mode="HTML"
+            )
+            try:
+
+                await query.edit_message_reply_markup(
+                    reply_markup=InlineKeyboardMarkup(kb_o))
+                # return succeeded , on_team, no_players
+                return False, False, no_players, total_players
+            except Exception as e:
+                print(e)
+            return False, False, no_players, total_players
+    else:
+        print("on List")
+        return False, False, no_players, total_players
+
+    # async def on_list(tid=498123938) -> int:
     tid = tid
     pb = PocketBase(os.environ['pb'])
 
-    p = pb.collection('team').get_list(1,20,{"filter": f'tid = {tid} && active = true'})
+    p = pb.collection('team').get_list(1, 20, {"filter": f'tid = {tid} && active = true'})
     return p.total_items
 
+
+async def completed(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /completed is issued."""
+    await update.message.delete()
+    info = await context.bot.send_message(
+        chat_id=update.message.chat_id,
+        text="Ending team listing session.",
+        parse_mode="HTML"
+    )
+    pb_db = Db()
+    if pb_db.active_seesion():
+        # remove inline keyboard from privious listing with message id from kulhun
+        try:
+            message_id = pb_db.pb.collection('kulhun').get_list(1, 20, {"filter": 'completed = false'}).items[
+                0].message_id
+            print(update.effective_chat.id)
+            print(update.message.chat_id)
+            await context.bot.edit_message_reply_markup(
+                chat_id=update.effective_chat.id,
+                message_id=message_id,
+                reply_markup=None
+            )
+        except Exception as e:
+            print(e)
+
+        # mark active session as completed
+        pb_db.pb.collection('kulhun').update(
+            pb_db.pb.collection('kulhun').get_list(1, 20, {"filter": 'completed = false'}).items[0].id,
+            {"completed": True})
+        await context.bot.send_message(
+            chat_id=update.message.chat_id,
+            text="No more team listing session is currently in progress.",
+            parse_mode="HTML"
+        )
+
+        # mark attended as true on all team
+        pb_db.all_attended()
+        await context.bot.send_message(
+            chat_id=update.message.chat_id,
+            text="All players marked as attended.",
+            parse_mode="HTML"
+        )
+
+        await context.bot.send_message(
+            chat_id=update.message.chat_id,
+            text="see you soon",
+            parse_mode="HTML"
+        )
+    else:
+        await context.bot.send_message(
+            chat_id=update.message.chat_id,
+            text="No active team listing session is currently in progress.",
+            parse_mode="HTML"
+        )
+
+    await info.delete()
+
+
+async def welcome_new_member(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    for new_member in update.message.new_chat_members:
+        # Send a welcome message to the group
+        uid = update.message.new_chat_members[0].id
+        print(new_member.id)
+        print(uid)
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f"Welcome {new_member.full_name} to the group!"
+        )
+        # Save new member
+        save = Db().insert_new_player(uid, new_member.full_name)
+        print(save)
+
+
+# Function to trigger when a bot is added to a group
+async def new_member(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print("new Group added")
+    gid = update.my_chat_member.chat.id
+    print(gid)
+    print(update.effective_chat.id)
+    # Leave the group
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"SORRY!\nYou are not Subscriber"
+    )
+    await context.bot.leave_chat(chat_id=gid)
+    await context.bot.send_message(
+        chat_id=498123938,
+        text=f"New group add Alerted\nGroup id = {gid}"
+    )
 
 
 def main() -> None:
@@ -149,7 +377,10 @@ def main() -> None:
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("team", team_maker))
+    application.add_handler(CommandHandler("completed", completed))
     application.add_handler(CallbackQueryHandler(inline_button))
+    application.add_handler(ChatMemberHandler(new_member, ChatMemberHandler.MY_CHAT_MEMBER))
+    application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, welcome_new_member))
 
     # on non command i.e message - echo the message on Telegram
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
--- services.py
+++ services.py
@@ -4,20 +4,69 @@ import os
 
 load_dotenv()
 
+
 class Db():
     def __init__(self):
         self.pb = PocketBase(os.environ['pb'])
 
+    def active_seesion(self) -> bool:
+        return True if self.pb.collection('kulhun').get_list(1, 20,
+                                                             {"filter": 'completed = false'}).total_items > 0 else False
+
+    def no_players(self) -> int:
+        return self.pb.collection('team').get_list(1, 20, {"filter": 'active = true'}).total_items
+
+    def max_players(self) -> int:
+        row = self.pb.collection('kulhun').get_list(1, 20, {"filter": 'completed = false'})
+        return row.items[0].max_players if row.total_items > 0 else 0
+
+    def max_reserved(self) -> int:
+        row = self.pb.collection('kulhun').get_list(1, 20, {"filter": 'completed = false'})
+        return row.items[0].max_reserves if row.total_items > 0 else 0
+
+    # mark all players attented for current seeion
+    def all_attended(self):
+        players = self.pb.collection('team').get_list(1, 20, {"filter": 'active = true'})
+        for player in players.items:
+            self.pb.collection('team').update(player.id, {"active": False})
+            self.pb.collection('team').update(player.id, {"attended": True})
+
+    def create_kulhun(self, tid: int, desc: str, mid: int, mp=14, mr=3):
+        # check for on goin polling
+        try:
+            p_sess = self.pb.collection('kulhun').get_list(1, 20, {"filter": 'completed = false'})
+            print('sess', p_sess.total_items)
+
+            # storing kulhun
+            if p_sess.total_items == 0:
+                body = {
+                    "tid": tid,
+                    "description": desc,
+                    "max_players": mp,
+                    "max_reserves": mr,
+                    "completed": False,
+                    "message_id": mid
+                }
+                self.pb.collection('kulhun').create(body)
+                return True
+            else:
+                return False
+        except Exception as e:
+            print("Error in some where")
+            print(e)
+
     def on_list(self, tid):
         p = self.pb.collection('team').get_list(1, 20, {"filter": f'tid = {tid} && active = true'})
         return p.total_items
+        # return 0
 
-    def add_to_team(self, tid):
+    def add_to_team(self, tid, on_team):
         try:
             body = {
                 "tid": tid,
                 "active": True,
                 "attended": False,
+                "on_team": on_team
 
             }
 
@@ -26,3 +75,52 @@ class Db():
         except Exception as e:
             print(f'In db uodate error {e}')
             return False
+
+    def insert_new_player(self, tid, name):
+        # insert new player to table player
+        print(name)
+        try:
+            body = {
+                "tid": tid,
+                "t_name": str(name)
+            }
+            self.pb.collection('players').create(body)
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+    def team_list(self) -> str:
+        players = self.pb.collection('team').get_list(1, 20, {"filter": 'active = true && on_team = true'})
+        reserved = self.pb.collection('team').get_list(1, 20, {"filter": 'active = true && on_team = false'})
+        team_list = ''
+        for i, playe in enumerate(players.items):
+            name = self.pb.collection('players').get_list(1, 20, {"filter": f'tid = {playe.tid}'}).items[0].t_name
+            team_list += f"{i + 1}. {name}\n"
+
+        if reserved.total_items > 0:
+            team_list += f"-------------------\nRESERVED\n-------------------\n"
+            for i, res in enumerate(reserved.items):
+                name = self.pb.collection('players').get_list(1, 20, {"filter": f'tid = {res.tid}'}).items[0].t_name
+                team_list += f"{i + 1}. {name}\n"
+        return team_list
+
+    # updates
+    # mark player off the list
+    def off_list(self, tid):
+        try:
+            players = self.pb.collection('team').get_list(1, 20, {"filter": f'active = true && tid={tid}'})
+            reserved = self.pb.collection('team').get_list(1, 20, {"filter": 'active = true && on_team = false'})
+            self.pb.collection('team').update(players.items[0].id, {"active": False})
+            if reserved.total_items > 0:
+                self.pb.collection('team').update(reserved.items[0].id, {"on_team": True})
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+
+if __name__ == "__main__":
+    pb = Db()
+    active = pb.team_list()
+    print(active)
