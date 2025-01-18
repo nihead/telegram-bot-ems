b26318f99d4767b99464082f403cf3d2b787e026
ninhub <nintoo663@gmail.com>
2025-01-18 15:46:17 +0500

    test init

--- profile_updated.py
+++ profile_updated.py
@@ -1,5 +1,5 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import CallbackContext, CallbackQueryHandler, MessageHandler, Filters, ContextTypes
+from telegram.ext import CallbackContext, CallbackQueryHandler, MessageHandler, Filters, ContextTypes, ConversationHandler, CommandHandler
 from telegram.constants import ParseMode, ChatAction
 
 
@@ -21,5 +21,24 @@ async def ask_mobile(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 async def ask_save(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     pass
 
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    query = update.callback_query
+    query.answer()
 
+    await query.edit_message_text(text="See you next time!")
+    return ConversationHandler.END
 
+def profile_update_handler() -> ConversationHandler:
+    return ConversationHandler(
+        entry_points=[CommandHandler("profile_update", ask_name)],
+        states={
+            ASK_NAME: [MessageHandler(Filters.text & ~Filters.command, ask_so)],
+            ASK_SO: [MessageHandler(Filters.text & ~Filters.command, ask_nickname)],
+            ASK_NICKNAME: [MessageHandler(Filters.text & ~Filters.command, ask_mobile)],
+            ASK_MOBILE: [MessageHandler(Filters.text & ~Filters.command, ask_save)],
+        },
+        fallbacks=[
+            CallbackQueryHandler(cancel, pattern="^cancel$"),
+            MessageHandler(Filters.text, cancel),
+        ],
+    )
